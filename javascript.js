28d796b5936679dba75510a3e131763733790e66
Adrienmaximus <adriencenik@gmail.com>
2024-07-04 09:01:43 +0200

    debut JS

---
+++ javascript.js
@@ -0,0 +1,19 @@
+var x = 5;
+
+var y = 10;
+var z = -2;
+
+x = x + 1 ;
+x = x - 2;
+
+y = y*2;
+
+var mult = x * y;
+
+var divi = y / z;
+
+var mod = 13 % 3;
+
+alert ("Variable mult : " + mult +
+"\nVariable div : "+ divi +
+"\nVariable mod : "+ mod );
