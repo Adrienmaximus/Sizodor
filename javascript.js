889afe1425277f302c5eeb32dd50b73bff65d83c
Adrienmaximus <adriencenik@gmail.com>
2024-07-18 12:38:04 +0200

    debut JS

--- javascript.js
+++ javascript.js
@@ -1,9 +1,8 @@
-var x = 5, y = 10, z = -2;
+function multiplication(x,y){
+    return x*y;
+}
+var resultat1 = multiplication(5,10);
+var resultat2 = multiplication(-4,60);
 
-x += 2;
-x -= 3;
-
-y *= x;
-y /= 2;
-y %=x;
-alert(x);
+resultat1 +=2;
+alert(resultat1);
